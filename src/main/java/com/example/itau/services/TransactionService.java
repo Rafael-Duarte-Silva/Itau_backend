f31f59f6414b5de0de82d471f30d7b5afc774c3b
Rafael-Duarte-Silva <rafaelddduante@gmail.com>
2026-04-12 18:08:42 -0300

    refactor: log setup

--- src/main/java/com/example/itau/services/TransactionService.java
+++ src/main/java/com/example/itau/services/TransactionService.java
@@ -3,49 +3,49 @@ package com.example.itau.services;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.example.itau.domain.Transaction;
 import com.example.itau.dtos.StatisticsDTO;
 import com.example.itau.repositories.TransactionRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class TransactionService {
 
     private final TransactionRepository repository;
-    private static final Logger logger = LoggerFactory.getLogger(TransactionService.class);
 
     public TransactionService(TransactionRepository repository) {
         this.repository = repository;
     }
 
     public boolean create(BigDecimal valor, OffsetDateTime dataHora) {
-        logger.debug("Creating transaction - valor={}, dataHora={}", valor, dataHora);
+        log.debug("Creating transaction - valor={}, dataHora={}", valor, dataHora);
 
         boolean isFuture = dataHora.isAfter(OffsetDateTime.now());
         if (isFuture || valor.signum() < 0) {
-            logger.warn("TRANSACTION rejected - invalid data. valor={}, dataHora={}", valor, dataHora);
+            log.warn("TRANSACTION rejected - invalid data. valor={}, dataHora={}", valor, dataHora);
             return false;
         }
 
         Transaction transaction = repository.save(new Transaction(valor, dataHora));
-        logger.info("TRANSACTION created - id={}", transaction.getId());
+        log.info("TRANSACTION created - id={}", transaction.getId());
 
         return true;
     }
 
     public void deleteAll() {
-        logger.warn("TRANSACTION delete all requested");
+        log.warn("TRANSACTION delete all requested");
         repository.deleteAll();
-        logger.info("TRANSACTION all deleted");
+        log.info("TRANSACTION all deleted");
     }
 
     public StatisticsDTO getStatistics() {
-        logger.debug("Fetching statistics");
+        log.debug("Fetching statistics");
         StatisticsDTO stats = repository.getStatistics();
-        logger.info("STATISTICS generated");
+        log.info("STATISTICS generated");
         return stats;
     }
 }
